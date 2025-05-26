82f0c4d68fc58254b4694c0cd8702fcc909c742b
William-Gabriel <willx_gabriel@yahoo.com>
2025-05-26 12:33:54 +0200

    Initial Commit

---
+++ ClassQuote/ClassQuoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClassQuoteApp.swift
+//  ClassQuote
+//
+//  Created by Perez William on 26/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct ClassQuoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClassQuoteTests/ClassQuoteTests.swift
@@ -0,0 +1,17 @@
+//
+//  ClassQuoteTests.swift
+//  ClassQuoteTests
+//
+//  Created by Perez William on 26/05/2025.
+//
+
+import Testing
+@testable import ClassQuote
+
+struct ClassQuoteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
